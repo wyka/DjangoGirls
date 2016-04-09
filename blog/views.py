4dd4526ea98aee2754b140cb214be57e131a65be
WK <168571@student.pwr.wroc.pl>
2016-04-09 18:46:09 +0200

    wyswietlanie detali postu

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_list(request):
     lista=Post.objects.all()
